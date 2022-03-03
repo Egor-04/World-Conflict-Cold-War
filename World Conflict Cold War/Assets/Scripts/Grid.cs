6318c87a9a7d18f3c09e29904044efe24d1ea1b2
Egor-04 <egor04m@gmail.com>
2022-03-04 00:59:03 +0300

    Update

---
+++ World Conflict Cold War/Assets/Scripts/Grid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private float _width, _height;
+    [SerializeField] private float _radius = 0.5f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
